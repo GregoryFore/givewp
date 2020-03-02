8bc785b1772efc6b7eeae4ed32b5c5e2cb684681
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-03-02 09:51:49 -0600

    feature: scaffold minichart reports

--- assets/src/js/admin/reports/utils/reports.js
+++ assets/src/js/admin/reports/utils/reports.js
@@ -88,314 +88,205 @@ export const getReport = ( { report, payments, period } ) => {
 				],
 			};
 		}
-		case 'total-income':
+		case 'total-income': {
+			const data = forPeriod( {
+				start: period.startDate,
+				end: period.endDate,
+				payments: filtered,
+				callback: ( { periodPayments, periodEnd } ) => {
+					const total = periodPayments.length ? periodPayments.reduce( ( t, c ) => {
+						return t + c.total;
+					}, 0 ) : 0;
+					const time = periodEnd.format();
+					return {
+						x: time,
+						y: total,
+					};
+				},
+			} );
+			const tooltips = forPeriod( {
+				start: period.startDate,
+				end: period.endDate,
+				payments: filtered,
+				callback: ( { periodPayments, periodEnd } ) => {
+					const total = periodPayments.length ? periodPayments.reduce( ( t, c ) => {
+						return t + c.total;
+					}, 0 ) : 0;
+					const time = periodEnd.format( 'MMM Do, Y' );
+					const donors = {};
+					periodPayments.forEach( ( payment ) => {
+						donors[ payment.donor.id ] = donors[ payment.donor.id ] ? donors[ payment.donor.id ] + 1 : 1;
+					} );
+
+					return {
+						title: total,
+						body: Object.keys( donors ).length + _n( ' Donor', ' Donors', Object.keys( donors ).length, 'give' ),
+						footer: time,
+					};
+				},
+			} );
+
 			return {
 				datasets: [
 					{
 						trend: -5,
 						highlight: '$150.00',
 						info: 'VS previous 7 days',
-						data: [
-							{
-								y: 20,
-								x: '2001-01-01',
-							},
-							{
-								y: 88,
-								x: '2001-01-02',
-							},
-							{
-								y: 88,
-								x: '2001-01-03',
-							},
-							{
-								y: 88,
-								x: '2001-01-04',
-							},
-							{
-								y: 88,
-								x: '2001-01-05',
-							},
-							{
-								y: 88,
-								x: '2001-01-06',
-							},
-							{
-								y: 88,
-								x: '2001-01-07',
-							},
-						],
-						tooltips: [
-							{
-								title: '$10.00',
-								body: '12 Donors',
-								footer: 'Jan 1',
-							},
-							{
-								title: '$10.00',
-								body: '12 Donors',
-								footer: 'Jan 2',
-							},
-							{
-								title: '$10.00',
-								body: '12 Donors',
-								footer: 'Jan 3',
-							},
-							{
-								title: '$10.00',
-								body: '12 Donors',
-								footer: 'Jan 4',
-							},
-							{
-								title: '$10.00',
-								body: '12 Donors',
-								footer: 'Jan 5',
-							},
-							{
-								title: '$20.00',
-								body: '3 Donors',
-								footer: 'Jan 6',
-							},
-							{
-								title: '$20.00',
-								body: '3 Donors',
-								footer: 'Jan 7',
-							},
-						],
+						data,
+						tooltips,
 					},
 				],
 			};
-		case 'total-donors':
+		}
+		case 'total-donors': {
+			const data = forPeriod( {
+				start: period.startDate,
+				end: period.endDate,
+				payments: filtered,
+				callback: ( { periodPayments, periodEnd } ) => {
+					const total = periodPayments.length ? periodPayments.reduce( ( t, c ) => {
+						return t + c.total;
+					}, 0 ) : 0;
+					const time = periodEnd.format();
+					return {
+						x: time,
+						y: total,
+					};
+				},
+			} );
+			const tooltips = forPeriod( {
+				start: period.startDate,
+				end: period.endDate,
+				payments: filtered,
+				callback: ( { periodPayments, periodEnd } ) => {
+					const total = periodPayments.length ? periodPayments.reduce( ( t, c ) => {
+						return t + c.total;
+					}, 0 ) : 0;
+					const time = periodEnd.format( 'MMM Do, Y' );
+					const donors = {};
+					periodPayments.forEach( ( payment ) => {
+						donors[ payment.donor.id ] = donors[ payment.donor.id ] ? donors[ payment.donor.id ] + 1 : 1;
+					} );
+
+					return {
+						title: total,
+						body: Object.keys( donors ).length + _n( ' Donor', ' Donors', Object.keys( donors ).length, 'give' ),
+						footer: time,
+					};
+				},
+			} );
 			return {
 				datasets: [
 					{
 						trend: -5,
 						highlight: '4',
 						info: 'VS previous 7 days',
-						data: [
-							{
-								y: 20,
-								x: '2001-01-01',
-							},
-							{
-								y: 88,
-								x: '2001-01-02',
-							},
-							{
-								y: 88,
-								x: '2001-01-03',
-							},
-							{
-								y: 88,
-								x: '2001-01-04',
-							},
-							{
-								y: 88,
-								x: '2001-01-05',
-							},
-							{
-								y: 88,
-								x: '2001-01-06',
-							},
-							{
-								y: 88,
-								x: '2001-01-07',
-							},
-						],
-						tooltips: [
-							{
-								title: '$10.00',
-								body: '12 Donors',
-								footer: 'Jan 1',
-							},
-							{
-								title: '$10.00',
-								body: '12 Donors',
-								footer: 'Jan 2',
-							},
-							{
-								title: '$10.00',
-								body: '12 Donors',
-								footer: 'Jan 3',
-							},
-							{
-								title: '$10.00',
-								body: '12 Donors',
-								footer: 'Jan 4',
-							},
-							{
-								title: '$10.00',
-								body: '12 Donors',
-								footer: 'Jan 5',
-							},
-							{
-								title: '$20.00',
-								body: '3 Donors',
-								footer: 'Jan 6',
-							},
-							{
-								title: '$20.00',
-								body: '3 Donors',
-								footer: 'Jan 7',
-							},
-						],
+						data,
+						tooltips,
 					},
 				],
 			};
-		case 'average-donation':
+		}
+		case 'average-donation': {
+			const data = forPeriod( {
+				start: period.startDate,
+				end: period.endDate,
+				payments: filtered,
+				callback: ( { periodPayments, periodEnd } ) => {
+					const total = periodPayments.length ? periodPayments.reduce( ( t, c ) => {
+						return t + c.total;
+					}, 0 ) : 0;
+					const time = periodEnd.format();
+					return {
+						x: time,
+						y: total,
+					};
+				},
+			} );
+			const tooltips = forPeriod( {
+				start: period.startDate,
+				end: period.endDate,
+				payments: filtered,
+				callback: ( { periodPayments, periodEnd } ) => {
+					const total = periodPayments.length ? periodPayments.reduce( ( t, c ) => {
+						return t + c.total;
+					}, 0 ) : 0;
+					const time = periodEnd.format( 'MMM Do, Y' );
+					const donors = {};
+					periodPayments.forEach( ( payment ) => {
+						donors[ payment.donor.id ] = donors[ payment.donor.id ] ? donors[ payment.donor.id ] + 1 : 1;
+					} );
+
+					return {
+						title: total,
+						body: Object.keys( donors ).length + _n( ' Donor', ' Donors', Object.keys( donors ).length, 'give' ),
+						footer: time,
+					};
+				},
+			} );
+
 			return {
 				datasets: [
 					{
 						trend: -5,
 						highlight: '$88.00',
 						info: 'VS previous 7 days',
-						data: [
-							{
-								y: 20,
-								x: '2001-01-01',
-							},
-							{
-								y: 88,
-								x: '2001-01-02',
-							},
-							{
-								y: 88,
-								x: '2001-01-03',
-							},
-							{
-								y: 88,
-								x: '2001-01-04',
-							},
-							{
-								y: 88,
-								x: '2001-01-05',
-							},
-							{
-								y: 88,
-								x: '2001-01-06',
-							},
-							{
-								y: 88,
-								x: '2001-01-07',
-							},
-						],
-						tooltips: [
-							{
-								title: '$10.00',
-								body: '12 Donors',
-								footer: 'Jan 1',
-							},
-							{
-								title: '$10.00',
-								body: '12 Donors',
-								footer: 'Jan 2',
-							},
-							{
-								title: '$10.00',
-								body: '12 Donors',
-								footer: 'Jan 3',
-							},
-							{
-								title: '$10.00',
-								body: '12 Donors',
-								footer: 'Jan 4',
-							},
-							{
-								title: '$10.00',
-								body: '12 Donors',
-								footer: 'Jan 5',
-							},
-							{
-								title: '$20.00',
-								body: '3 Donors',
-								footer: 'Jan 6',
-							},
-							{
-								title: '$20.00',
-								body: '3 Donors',
-								footer: 'Jan 7',
-							},
-						],
+						data,
+						tooltips,
 					},
 				],
 			};
-		case 'total-refunds':
+		}
+		case 'total-refunds': {
+			const data = forPeriod( {
+				start: period.startDate,
+				end: period.endDate,
+				payments: filtered,
+				callback: ( { periodPayments, periodEnd } ) => {
+					const total = periodPayments.length ? periodPayments.reduce( ( t, c ) => {
+						return t + c.total;
+					}, 0 ) : 0;
+					const time = periodEnd.format();
+					return {
+						x: time,
+						y: total,
+					};
+				},
+			} );
+			const tooltips = forPeriod( {
+				start: period.startDate,
+				end: period.endDate,
+				payments: filtered,
+				callback: ( { periodPayments, periodEnd } ) => {
+					const total = periodPayments.length ? periodPayments.reduce( ( t, c ) => {
+						return t + c.total;
+					}, 0 ) : 0;
+					const time = periodEnd.format( 'MMM Do, Y' );
+					const donors = {};
+					periodPayments.forEach( ( payment ) => {
+						donors[ payment.donor.id ] = donors[ payment.donor.id ] ? donors[ payment.donor.id ] + 1 : 1;
+					} );
+
+					return {
+						title: total,
+						body: Object.keys( donors ).length + _n( ' Donor', ' Donors', Object.keys( donors ).length, 'give' ),
+						footer: time,
+					};
+				},
+			} );
+
 			return {
 				datasets: [
 					{
 						trend: -5,
 						highlight: '9',
 						info: 'VS previous 7 days',
-						data: [
-							{
-								y: 20,
-								x: '2001-01-01',
-							},
-							{
-								y: 88,
-								x: '2001-01-02',
-							},
-							{
-								y: 88,
-								x: '2001-01-03',
-							},
-							{
-								y: 88,
-								x: '2001-01-04',
-							},
-							{
-								y: 88,
-								x: '2001-01-05',
-							},
-							{
-								y: 88,
-								x: '2001-01-06',
-							},
-							{
-								y: 88,
-								x: '2001-01-07',
-							},
-						],
-						tooltips: [
-							{
-								title: '$10.00',
-								body: '12 Donors',
-								footer: 'Jan 1',
-							},
-							{
-								title: '$10.00',
-								body: '12 Donors',
-								footer: 'Jan 2',
-							},
-							{
-								title: '$10.00',
-								body: '12 Donors',
-								footer: 'Jan 3',
-							},
-							{
-								title: '$10.00',
-								body: '12 Donors',
-								footer: 'Jan 4',
-							},
-							{
-								title: '$10.00',
-								body: '12 Donors',
-								footer: 'Jan 5',
-							},
-							{
-								title: '$20.00',
-								body: '3 Donors',
-								footer: 'Jan 6',
-							},
-							{
-								title: '$20.00',
-								body: '3 Donors',
-								footer: 'Jan 7',
-							},
-						],
+						data,
+						tooltips,
 					},
 				],
 			};
+		}
 		case 'income-breakdown': {
 			const rows = forPeriod( {
 				start: period.startDate,
